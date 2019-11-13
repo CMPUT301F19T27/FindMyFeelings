dc565a797873543be98392534a09ee6cb271a86a
shearpaladin <shearpaladin@gmail.com>
2019-11-13 03:26:53 -0700

    COME On

--- app/src/test/java/com/example/findmyfeelings/MoodTest.java
+++ app/src/test/java/com/example/findmyfeelings/MoodTest.java
@@ -70,100 +70,22 @@ public class MoodTest {
 
 
     }
-/*
-    @Test
-    public void testHasCity(){
-        CityList cityList = mockCityList();
-
-        assertEquals(0,mockCity().compareTo(cityList.getCities().get(0))); // Comparing Edmonton == Edmonton
-        assertEquals(1, cityList.getCities().size()); //Edmonton should in in list
-
-        assertTrue((cityList.hasCity(cityList.getCities().get(0)))); // Check for Edmonton
-
-        City city = new City("Vancouver","BC");
-        cityList.add(city);
-        assertTrue((cityList.hasCity(city))); // Check for BC
-
-        City cityFake = new City("Wall","Street");
-        assertFalse((cityList.hasCity(cityFake))); // Check for Wall Street
-
-
-    }
 
 
     @Test
     public void testDelete(){
-        CityList cityList = mockCityList();
 
-        assertEquals(1, cityList.getCities().size()); //Edmonton should in in list
+        ArrayList<Mood> moodList= mockMoodList();
+        assertEquals(1, moodList.size()); // Should have 1 mood
 
-        cityList.delete(cityList.getCities().get(0)); // Removing Edmonton from list
+        moodList.remove(0);
 
-        assertEquals(0, cityList.getCities().size()); // List is empty
-
-        City city = new City("Vancouver","BC");
-        City city2 = new City("Wall","Street");
-        cityList.add(city); // add Vancouver
-        cityList.add(city2); // add Wall
-
-        assertEquals(2, cityList.getCities().size()); // Size should be 2
-
-        cityList.delete(city); // Deleting Vancouver
-
-        assertEquals(1, cityList.getCities().size()); // Size should be 1
-
-        assertFalse(cityList.hasCity(city)); // Check if Vancouver is still in list should return false
-
+        assertEquals(0, moodList.size()); // List is empty
 
 
     }
 
-
-    @Test
-    public void testDeleteException(){
-        final CityList cityList = mockCityList();
-
-        City city = new City("Yellowknife","Northwest Territories");
-
-        cityList.add(city);
-
-        assertEquals(2, cityList.getCities().size()); // Edmonton, Yellowknife
-
-        cityList.delete(city); // Delete Yellowknife
-
-        assertEquals(1, cityList.getCities().size()); // Edmonton
-
-        assertThrows(IllegalArgumentException.class, ()->{
-            cityList.delete(city);  // throw exception if we try to delete Yellowknife again
-
-        });
-
-        assertFalse(cityList.hasCity(city)); // check if Yellowknife still exists (should not be!)
-
-    }
-
-    @Test
-    public void testCountCities(){
-        CityList cityList = mockCityList();
-
-        assertEquals(1, cityList.countCities()); // 1 == 1
-
-        City city = new City ("Galaxy", "Land");
-        cityList.add(city); // size = 2
-
-        assertEquals(2, cityList.countCities()); // 2 == 2
-
-        cityList.delete(city);
-        assertEquals(1, cityList.countCities()); // 1 == 1
-
-        cityList.delete(cityList.getCities().get(0));
-
-        assertEquals(0, cityList.countCities()); // 0 == 0
-
-    }
-
-
-*/
+    
 
 
 }
