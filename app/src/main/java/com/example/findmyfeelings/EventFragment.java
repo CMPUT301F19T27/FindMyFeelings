533eb495f91acc80e7d9aa60fb9c26b07a920e4b
cameronhildebrandt <cameronhildebrandt1@gmail.com>
2019-11-21 12:19:01 -0700

    Updated the GUI for adding a mood

--- app/src/main/java/com/example/findmyfeelings/EventFragment.java
+++ app/src/main/java/com/example/findmyfeelings/EventFragment.java
@@ -13,6 +13,10 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -26,6 +30,7 @@ import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -37,15 +42,24 @@ import java.util.List;
 public class EventFragment extends DialogFragment  {
     private static final String ARG_MOOD = "mood";
     private static final String ARG_INDEX = "index";
-    
+
+    private ImageView happy;
+    private ImageView sad;
+    private ImageView angry;
+    private ImageView disgusted;
+    private ImageView surprised;
+    private ImageView scared;
+    private String moodSelected = "";
     private EditText moodType;
     private EditText moodDate;
     private EditText moodTime;
     private EditText moodReason;
 
+    private RadioGroup radioSituationGroup;
+    private RadioButton radioSituationButton;
+
     private EditText moodSituation;
     private CheckBox checkLocation;
-
 
     private OnFragmentInteractionListener listener;
     private Mood currentMood;
@@ -75,7 +89,7 @@ public class EventFragment extends DialogFragment  {
     }
 
     /**
-     * intializes the context for listener
+     * Initialises the context for listener
      * @param context
      */
     @Override
@@ -89,6 +103,7 @@ public class EventFragment extends DialogFragment  {
         }
     }
 
+
     /**
      * This method creates a dialog and shows a dialog
      */
@@ -97,11 +112,19 @@ public class EventFragment extends DialogFragment  {
     public Dialog onCreateDialog(@Nullable Bundle savedInstanceState){
 
         View view= LayoutInflater.from(getActivity()).inflate(R.layout.fragment_event, null);
-        moodType = view.findViewById(R.id.mood_type_editText);
-        moodDate = view.findViewById(R.id.mood_date_editText);
-        moodTime = view.findViewById(R.id.mood_time_editText);
+
+        happy = view.findViewById(R.id.happy_emoticon);
+        sad = view.findViewById(R.id.sad_emoticon);
+        angry = view.findViewById(R.id.angry_emoticon);
+        disgusted = view.findViewById(R.id.disgusted_emoticon);
+        surprised = view.findViewById(R.id.surprised_emoticon);
+        scared = view.findViewById(R.id.scared_emoticon);
+
+//        moodType = view.findViewById(R.id.mood_type_editText);
+//        moodDate = view.findViewById(R.id.mood_date_editText);
+//        moodTime = view.findViewById(R.id.mood_time_editText);
         moodReason = view.findViewById(R.id.mood_reason_editText);
-        moodSituation = view.findViewById(R.id.mood_situation_editText);
+//        moodSituation = view.findViewById(R.id.mood_situation_editText);
 
         checkLocation = view.findViewById(R.id.location_check);
 
@@ -120,15 +143,96 @@ public class EventFragment extends DialogFragment  {
             String time = timeFormat.format(currentMood.getDateTime());
 
             moodType.setText(currentMood.getMood());
-            moodDate.setText(date);
-            moodTime.setText(time);
+//            moodDate.setText(date); //TODO make it set the custom date
+//            moodTime.setText(time); //TODO make it set the custom time
             moodReason.setText(currentMood.getReason());
             moodSituation.setText(currentMood.getSituation());
             if (currentMood.getLocation() != null) {
                 checkLocation.setChecked(true);
             }
-
         }
+
+        happy.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                happy.setImageResource(R.drawable.happy_face);
+                sad.setImageResource(R.drawable.sad_face_bw);
+                angry.setImageResource(R.drawable.angry_face_bw);
+                disgusted.setImageResource(R.drawable.disgust_face_bw);
+                surprised.setImageResource(R.drawable.surprised_face_bw);
+                scared.setImageResource(R.drawable.fear_face_bw);
+
+                moodSelected = "Happy";
+            }
+        });
+
+        sad.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                happy.setImageResource(R.drawable.happy_face_bw);
+                sad.setImageResource(R.drawable.sad_face);
+                angry.setImageResource(R.drawable.angry_face_bw);
+                disgusted.setImageResource(R.drawable.disgust_face_bw);
+                surprised.setImageResource(R.drawable.surprised_face_bw);
+                scared.setImageResource(R.drawable.fear_face_bw);
+
+                moodSelected = "Sad";
+            }
+        });
+
+        angry.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                happy.setImageResource(R.drawable.happy_face_bw);
+                sad.setImageResource(R.drawable.sad_face_bw);
+                angry.setImageResource(R.drawable.angry_face);
+                disgusted.setImageResource(R.drawable.disgust_face_bw);
+                surprised.setImageResource(R.drawable.surprised_face_bw);
+                scared.setImageResource(R.drawable.fear_face_bw);
+
+                moodSelected = "Angry";
+            }
+        });
+
+        disgusted.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                happy.setImageResource(R.drawable.happy_face_bw);
+                sad.setImageResource(R.drawable.sad_face_bw);
+                angry.setImageResource(R.drawable.angry_face_bw);
+                disgusted.setImageResource(R.drawable.disgust_face);
+                surprised.setImageResource(R.drawable.surprised_face_bw);
+                scared.setImageResource(R.drawable.fear_face_bw);
+
+                moodSelected = "Disgusted";
+            }
+        });
+
+        surprised.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                happy.setImageResource(R.drawable.happy_face_bw);
+                sad.setImageResource(R.drawable.sad_face_bw);
+                angry.setImageResource(R.drawable.angry_face_bw);
+                disgusted.setImageResource(R.drawable.disgust_face_bw);
+                surprised.setImageResource(R.drawable.surprised_face);
+                scared.setImageResource(R.drawable.fear_face_bw);
+
+                moodSelected = "Surprised";
+            }
+        });
+
+        scared.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                happy.setImageResource(R.drawable.happy_face_bw);
+                sad.setImageResource(R.drawable.sad_face_bw);
+                angry.setImageResource(R.drawable.angry_face_bw);
+                disgusted.setImageResource(R.drawable.disgust_face_bw);
+                surprised.setImageResource(R.drawable.surprised_face_bw);
+                scared.setImageResource(R.drawable.fear_face);
+
+                moodSelected = "Scared";
+            }
+        });
+
+
+        addListenerOnButton();
+
 
         final AlertDialog builder = new AlertDialog.Builder(getContext())
                 .setView(view)
@@ -154,20 +258,16 @@ public class EventFragment extends DialogFragment  {
                     public void onClick(View view) {
 
                         boolean flag = false;
-                        String[] moods = new String[]{"Happy", "Sad", "Angry", "Disgusted", "Surprised", "Scared"};
-                        List<String> validMoods = Arrays.asList(moods);
+//                        String[] moods = new String[]{"Happy", "Sad", "Angry", "Disgusted", "Surprised", "Scared"};
+//                        List<String> validMoods = Arrays.asList(moods);
 
                         String[] situations = new String[]{"alone", "Alone", "with 1", "with 2", "With 1", "With 2", "crowd", "Crowd"};
 
                         List<String> validSituations = Arrays.asList(situations);
 
-                        if (moodType.getText().toString().length() == 0) {
+                        if (moodSelected.equals("")) {
                             flag = true;
-                            moodType.setError("Enter a mood!");
-                        }
-                        if (!(validMoods.contains(moodType.getText().toString()))) {
-                            flag = true;
-                            moodType.setError("Please enter a valid mood");
+                            moodType.setError("Select a mood!");
                         }
 
                         if (moodDate.getText().toString().length() == 0) {
@@ -178,6 +278,7 @@ public class EventFragment extends DialogFragment  {
                             flag = true;
                             moodDate.setError("Enter a valid date (yyyy-MM-dd)!");
                         }
+
                         if (moodTime.getText().toString().length() == 0) {
                             flag = true;
                             moodTime.setError("Enter a time!");
@@ -186,17 +287,17 @@ public class EventFragment extends DialogFragment  {
                             flag = true;
                             moodTime.setError("Enter a valid time (HH:mm)!");
                         }
-                        if (moodSituation.getText().toString().length() == 0){
+
+                        if (moodSituation.getText().toString().length() == 0) {
                             flag = true;
                             moodSituation.setError("Enter a situation!");
                         }
-
                         if (!(validSituations.contains(moodSituation.getText().toString()))) {
                             flag = true;
                             moodSituation.setError("Alone, With 1, With 2 or more, Crowd");
                         }
 
-                        if (flag == false) {
+                        if (!flag) {
                             System.out.println(moodDate+" "+ moodTime);
 
                             Date dateTime = null;
@@ -206,8 +307,7 @@ public class EventFragment extends DialogFragment  {
                                 e.printStackTrace();
                             }
 
-                            String newMood = moodType.getText().toString();
-
+                            String newMood = moodSelected;
                             String situation = moodSituation.getText().toString();
                             String reason = moodReason.getText().toString();
                             String moodId = newMood + dateTime.toString();
@@ -234,6 +334,32 @@ public class EventFragment extends DialogFragment  {
             }
         });
         return builder;
+    }
+
+    public void addListenerOnButton() {
+        View view= LayoutInflater.from(getActivity()).inflate(R.layout.fragment_event, null);
+
+        radioSituationGroup = view.findViewById(R.id.radio);
+//        tempButton = view.findViewById(R.id.btnDisplay);
+
+//        tempButton.setOnClickListener(new View.OnClickListener() {
+//
+//            @Override
+//            public void onClick(View v) {
+//
+//                // get selected radio button from radioGroup
+//                int selectedId = radioSituationGroup.getCheckedRadioButtonId();
+//
+//                // find the radiobutton by returned id
+//                radioSituationButton = view.findViewById(selectedId);
+//
+////                Toast.makeText(getActivity(), radioSituationButton.getText(), Toast.LENGTH_SHORT).show();
+//                Toast.makeText(getActivity(), "Hi", Toast.LENGTH_SHORT).show();
+////                tempButton.setText(radioSituationButton.getText());
+//            }
+//
+//        });
+
     }
 
     /**
